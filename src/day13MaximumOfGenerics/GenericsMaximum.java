9cddd164fecd2bc7df661cb40f2e46e8ca52b314
Hiteshmeher <meher.hitesh4@gmail.com>
2022-07-11 23:35:48 +0530

    added max method to take more parameters

--- src/day13MaximumOfGenerics/GenericsMaximum.java
+++ src/day13MaximumOfGenerics/GenericsMaximum.java
@@ -7,39 +7,23 @@ package day13MaximumOfGenerics;
  */
 
 public class GenericsMaximum<T extends Comparable> {
-	T x;
-	T y;
-	T z;
-/**
- * 
- * @param x
- * @param y
- * @param z
- */
-	public GenericsMaximum(T x, T y, T z) {  //Constructors
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
 
-	public static <T extends Comparable> T toPrintMax(T x, T y, T z) {
+	public static <T extends Comparable> T maximum(T w, T x, T y, T z) {
 		/**
-		 * 
-		 * @param x taking first value
-		 * @param y taking second value
-		 * @param z taking third value
+		 * @param w taking first value
+		 * @param x taking second value
+		 * @param y taking third value
+		 * @param z taking fourth value
 		 * @return returning the result which is maximum
 		 */
 
-		T max = x;
-
-		if (y.compareTo(max) > 0 && y.compareTo(z) > 0) {
+		T max = w;
+		if (x.compareTo(max) > 0)
+			max = x;
+		if (y.compareTo(max) > 0)
 			max = y;
-		}
-		if (z.compareTo(max) > 0) {
+		if (z.compareTo(max) > 0)
 			max = z;
-		}
 		return max;
 	}
-
 }
